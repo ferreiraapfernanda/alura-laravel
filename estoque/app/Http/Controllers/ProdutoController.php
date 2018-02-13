627c7649d4ca799bb0149e73bd93eb0396f02220
Fernanda Aparecida <ferreira.aparecidaf2gmail.com>
2018-02-13 18:34:36 -0200

    Laravel Aula 06

--- estoque/app/Http/Controllers/ProdutoController.php
+++ estoque/app/Http/Controllers/ProdutoController.php
@@ -20,4 +20,23 @@ class ProdutoController extends Controller{
         
         return view('produto.detalhes')->with('p', $produto[0]);
     }
+
+    public function novo() {
+        return view('produto.formulario');
+    }
+
+    public function adiciona(){
+
+        //pegar informações do form
+        $nome = Request::input('nome');
+        $valor = Request::input('valor');
+        $quantidade = Request::input('quantidade');
+        $descricao = Request::input('descricao');
+
+        //salvar no bd
+        DB::insert('insert into produtos (nome, valor, quantidade, descricao) values (?,?,?,?)', 
+            array($nome, $valor, $quantidade, $descricao));
+
+        return view('produto.adicionado')->with('nome', $nome);
+    }
 }
